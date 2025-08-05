7643de4b3dd98541c8f85c42a8e2d0a43248a962
gabrielluz23 <gabriellok1212@gmail.com>
2025-08-05 10:11:09 -0300

    removing past

--- test/clojure_exercism/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure-exercism.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-exercism.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
